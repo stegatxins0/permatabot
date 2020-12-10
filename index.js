a4b7ec874c0c735a238b263abcab60455a2841e9
stegatxins0 <zero@stegatxins.xyz>
2020-12-10 11:55:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", function (){
-    console.log(`${bot.user.username} Is Online!`);
+    console.log(`${client.user.username} Is Online!`);
 });
 
 client.login(process.env.token);
