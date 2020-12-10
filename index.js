f239e63daf5faa92bc46d1387effb7dd494645bf
stegatxins0 <zero@stegatxins.xyz>
2020-12-10 12:16:33 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
+const fs = require('fs');
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } 
+});
 
 client.on("ready", function (){
     console.log(`${client.user.username} Is Online!`);
