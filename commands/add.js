0673c57fae0f27c606a7e6b4cdbbb2a833d55013
stegatxins0 <zero@stegatxins.xyz>
2020-12-10 13:48:01 +0800

    1

--- commands/add.js
+++ commands/add.js
@@ -2,9 +2,22 @@ module.exports = {
     name: 'add',
     description: "add assignments",
     execute(message, args){
-        let rm_time = args[0]
-        let rm_role = args[1]
-        let rm_message = args.slice(2).join("")
+        var rm_time = [];
+        var i;
+        for (i in args){
+            if (i.startsWith('@')) {
+                var rm_role = i;
+                delete args[0];
+                rm_msg = args.join(" ");
+                break;
+            } else {
+                rm_time.pop(i);       
+            }
         message.channel.send(rm_time + "\n" + rm_role + "\n" + rm_message);
+        } 
+        // const rm_role = message.mentions.users.first()
+        // let rm_time = args[0]
+        // let rm_role = args[1]
+        // let rm_message = args.slice(2).join("")
     }
 }
