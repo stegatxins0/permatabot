9459ab8134704ce9db613321552d511e63936deb
stegatxins0 <zero@stegatxins.xyz>
2020-12-12 21:59:18 +0800

    add ping remind

--- commands/list.js
+++ commands/list.js
@@ -12,13 +12,13 @@ module.exports = {
                 var dtame = moment(list.time, 'YYYY-MM-DD hh:mm')
                 if (dtame.diff(moment(), 'hours') < 24 && list.checkremind != 24 && list.check !=3){
                     sq.sq_update(username, list.time, list.assignment, 24)
-                    client.channels.cache.get(config.reminders.remindchannel).send(`**1 DAY LEFT WARNING**: Your assignment ${list.assignment} is due at ${list.time}`)
+                    client.channels.cache.get(config.reminders.remindchannel).send(`*<@${mbrid}>, *1 DAY LEFT WARNING**: Your assignment ${list.assignment} is due at ${list.time}`)
                 }else if (dtame.diff(moment(), 'hours') < 3 && list.checkremind !=3){
                     sq.sq_update(username, list.time, list.assignment, 3)
-                    client.channels.cache.get(config.reminders.remindchannel).send(`**FINAL WARNING**: Your assignment ${list.assignment} is due at ${list.time}`)
+                    client.channels.cache.get(config.reminders.remindchannel).send(`*<@${mbrid}>, **FINAL WARNING**: Your assignment ${list.assignment} is due at ${list.time}`)
                 }else if (dtame.diff(moment()) < 0){
                     sq.sq_delete(username, list.time, list.assignment)
-                    client.channels.cache.get(config.reminders.remindchannel).send(`**REMOVING ${list.assignment}**: Prepare an excuse if you haven't done ${list.assignment} which was due at ${list.time}`)
+                    client.channels.cache.get(config.reminders.remindchannel).send(`*<@${mbrid}>, **REMOVING ${list.assignment}**: Prepare an excuse if you haven't done ${list.assignment} which was due at ${list.time}`)
                 }
                 var friendlydate = dtame.fromNow();
                 let msg = await message.channel.send(`**${friendlydate}** | *${list.time}* ➤ ${list.assignment}`) 
