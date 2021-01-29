c46fe9affd76fb9260cbc83b32277aea59596c4b
stegatxins0 <zero@stegatxins.xyz>
2021-01-29 22:48:46 +0800

    stable version released

--- commands/add.js
+++ commands/add.js
@@ -1,76 +1,41 @@
-const moment = require("moment");
-const sq = require("../update");
-const delay = ms => new Promise(res => setTimeout(res, ms));
-
+// const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3')
+db = new sqlite3.Database('./assignment.db')
+const moment = require('moment')
 module.exports = {
     name: 'add',
-    format: '*add time @user/role assignment*',
-    description: "Please follow the format. DO NOT USE COMMAND THAT DOESN'T FOLLOW THIS FORMAT OR THE BOT WILL CRASH",
-    async execute(message, args, Discord){
-        everyones = false
-        const helpmsg = new Discord.MessageEmbed()
-            .setColor('#7FE5F0')
-            .setTitle("ADD ASSIGNMENT")
-            .setDescription(this.description)
-            .addField("Format", this.format)
-            .addFields(
-                { name: "Time", value: `Time currently only support the format **12 Dec 2020 23:59**. If time is not mentioned it will default to 1200. If year is not mentioned it will default to 1970?.`},
-                { name: "Ping", value: `*@2k1 or @aqil*. You can only ping one user or role. The assignment will be added to all **ONLINE MEMBERS** of pinged role only. Do not ping @here`},
-                { name: "Assignment", value: `Insert any text you want. Do not use weird symbol such as <, # or @`}
-                )
-        rm_cmd = args.join(" ")
-        function add_to_role(list, member){
-            if(list){
-            for (role in member){
-                sq.sq_add(member[role], list)
+    description: "add assignment with .add *user* *date* *assignment*",
+    execute(message, args){
+        function err(){message.channel.send("Invalid date. Do not have space in between date.")}
+        user = args[0];
+        subject = args.slice(2).join(" ");
+        date = moment(args[1]).set({"year": 2021});
+        function sqlite_add(username,masterlist){
+            db.run(`CREATE TABLE IF NOT EXISTS ${username} (time DATETIME, assignment TEXT, checkremind INTEGER)`,function(err){
+                if (err){
+                    console.log(err)
+                    message.channel.send("This is a sqlite3 error. If you are a developer, please check console for more details.")
+                }else{
+                    db.run(`INSERT INTO ${username} (time, assignment) VALUES (?, ?)`, masterlist);
+                    message.channel.send(`Assignment for ${username} added.`)
+                }
+            })
+        }
+        function add(){
+            if (moment(date).format('HH:mm') == "00:00") {date = date.set({"hour": 23, "minute": 59});}
+            masterlist = [date.format('YYYY-MM-DD HH:mm'), subject]
+            userlist = []
+            if (user == "all"){ userlist.push("jack", "cw", "aqil")
+            }else if (user == "moral"){ userlist.push("jack", "cw")
+            }else if (user == "jack"){ userlist.push("jack")
+            }else if (user == "aqil"){ userlist.push("aqil")
+            }else if (user == "cw"){ userlist.push("cw")}
+            if (userlist.length == 0){message.channel.send("Who are you? Your name is invalid")}else{
+            for (usersin in userlist){
+                sqlite_add(userlist[usersin], masterlist)
             }
             }
         }
-        function rm_parse(args){
-            if(everyones == true){
-                rm_time = rm_cmd.substr(0,rm_cmd.indexOf("@")).trim();
-                rm_message = rm_cmd.substr(rm_cmd.indexOf("@")+9).trim();
-            }else{
-                rm_time = rm_cmd.substr(0,rm_cmd.indexOf("<")).trim();
-                rm_message = rm_cmd.substr(rm_cmd.lastIndexOf(">")+1).trim();
-            }
-            rm_time = moment(rm_time).format("YYYY-MM-DD HH:mm");
-            validcheck = moment(rm_time, "YYYY-MM-DD HH:mm").isValid()
-            if (validcheck != true){
-                message.reply("Time is invalid. Please use 2020 Dec 12 23:59");
-                return;
-            }
-            rm_list = [];
-            rm_list.push(rm_time, rm_message);
-            return rm_list;
-        }
-        
-        function rm_member(message){
-            let memberlist = []
-            let role = message.mentions.roles.first();
-            if (message.mentions.members.first()) {
-                memberlist.push(message.mentions.members.first().id)
-            } else if (message.mentions.roles.first()) {
-                role.members.forEach(user => {
-                    memberlist.push(user.user.id);
-                });
-            } else if (rm_cmd.includes("@everyone")) {
-                everyones = true
-                message.guild.members.cache.forEach(user => {
-                    memberlist.push(user.user.id);
-                });
-            } else {
-                message.reply(helpmsg);
-                return;
-            }
-            return memberlist;
-        }
-        if(!args[0] || !args[1] || !args[2]){message.reply(helpmsg)}
-        else{
-            mbrfst = rm_member(message)
-            add_to_role(rm_parse(args), mbrfst)
-            await delay(3000)
-            message.reply("Done. 🙂")
-        }  
-    } 
+        if (user){if (date.isValid() && subject){add()}else{err()}}else{message.channel.send("add assignment with .add *user* *date* *assignment*")}
     }
+}
